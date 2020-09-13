0168c8bca56f5baade4bb9a13f023fb728e36908
Debojyoti Majumder <debojyoti.majumder@gmail.com>
2020-09-13 10:36:00 +0530

    Created a basic express Application

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(
+    PORT, 
+    console.log(`Application running on ${PORT}`)
+);
